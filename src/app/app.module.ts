d84fe82a339dd2e9298e3a58f6920c242c8ee1c7
Soumadiptya1997 <soumadiptyas23@gmail.com>
2021-06-21 14:38:31 +0530

    Routing module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,50 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
+import { AppRoutingModule, routingComponent } from './app-routing.module';
 
-// Compontents
+// App Compontent
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { SearchPinComponent } from './search-pin/search-pin.component';
-import { SearchDistrictComponent } from './search-district/search-district.component';
-import { RegisterComponent } from './register/register.component';
-import { SearchContentComponent } from './search-content/search-content.component';
-import { FaqComponent } from './faq/faq.component';
-import { RegistrationComComponent } from './registration-com/registration-com.component';
-import { SchedulingComComponent } from './scheduling-com/scheduling-com.component';
-import { VaccinationComComponent } from './vaccination-com/vaccination-com.component';
-import { CertificateComComponent } from './certificate-com/certificate-com.component';
-import { SideeffectComComponent } from './sideeffect-com/sideeffect-com.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HomePageComponent,
-    SearchPinComponent,
-    SearchDistrictComponent,
-    RegisterComponent,
-    SearchContentComponent,
-    FaqComponent,
-    RegistrationComComponent,
-    SchedulingComComponent,
-    VaccinationComComponent,
-    CertificateComComponent,
-    SideeffectComComponent,
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: 'home', component: HomePageComponent },
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'login', component: LoginComponent },
-      { path: 'register', component: RegisterComponent },
-      { path: 'faq', component: FaqComponent },
-    ]),
-  ],
+  declarations: [routingComponent, AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
 
   providers: [],
   bootstrap: [AppComponent],
