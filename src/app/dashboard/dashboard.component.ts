12dcc614e6388c56bb7b4fab7761668eae6a5e4d
sayanti612 <sayanti612@gmail.com>
2021-06-25 10:18:18 +0530

    narkol stopped work

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,11 +9,11 @@ export class DashboardComponent implements OnInit {
   constructor() {}
   ngOnInit(): void {}
 
-  first_name: string = 'Sudip';
-  last_name: string = 'Purakayet';
-  refid: string = '44816095211380 ';
-  secret_id: string = '1380';
-  birth_year: string = '1997';
-  photoid: string = 'Voter Card';
-  id_no: string = 'ABC123';
+  // first_name: string = 'Sudip';
+  // last_name: string = 'Purakayet';
+  // refid: string = '44816095211380 ';
+  // secret_id: string = '1380';
+  // birth_year: string = '1997';
+  // photoid: string = 'Voter Card';
+  // id_no: string = 'ABC123';
 }
