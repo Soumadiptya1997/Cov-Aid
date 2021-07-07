f38ea6787cf8798da65e4c35f58abefe32a8922c
Soumadiptya1997 <soumadiptyas23@gmail.com>
2021-07-07 12:58:35 +0530

    login logout problem partiallysolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Cov-Aid';
+  public CovLog: any;
+  public loginFlag: boolean = false;
+  constructor(public cookieS: CookieService) {
+    this.CovLog = this.cookieS.get('LOGINCOV');
+    console.log(this.CovLog);
+    if (this.cookieS.check('LOGINOV')) {
+      this.loginFlag = true;
+    } else {
+      this.loginFlag = false;
+    }
+  }
+  loginFunc() {
+    this.cookieS.set('LOGINCOV', 'COVAID');
+    this.loginFlag = true;
+  }
 }
--- src/app/otp-page/otp-page.component.ts
+++ src/app/otp-page/otp-page.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-otp-page',
   templateUrl: './otp-page.component.html',
-  styleUrls: ['./otp-page.component.css']
+  styleUrls: ['./otp-page.component.css'],
 })
 export class OtpPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
